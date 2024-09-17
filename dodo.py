3c19414120359ee0dabded3f0e04958081289181
prakaa <abiprakash007@gmail.com>
2024-09-17 13:21:07 +1000

    add doit task for caching required tables

---
+++ dodo.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+from ispypsa.data_fetch.local_cache import REQUIRED_TABLES, build_local_cache
+
+
+def build_parsed_workbook_cache(cache_location: Path):
+    workbook_path = list(
+        Path(Path.cwd().parent, "isp-workbook-parser", "workbooks", "6.0").glob(
+            "*.xlsx"
+        )
+    ).pop()
+    build_local_cache(cache_location, workbook_path)
+
+
+def task_cache_required_tables():
+    cache_location = Path("model_inputs", "workbook_table_cache")
+    return {
+        "actions": [(build_parsed_workbook_cache, [cache_location])],
+        "targets": [Path(cache_location, table + ".csv") for table in REQUIRED_TABLES],
+        # force doit to always mark the task as up-to-date (unless target removed)
+        "uptodate": [True],
+    }
