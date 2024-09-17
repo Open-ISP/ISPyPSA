894221e89713b95516c0d6f27c9809806b3a471a
prakaa <abiprakash007@gmail.com>
2024-09-17 12:26:01 +1000

    add config for package logging

---
+++ src/ispypsa/__init__.py
@@ -0,0 +1,7 @@
+import logging
+import sys
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO, format="%(levelname)s: %(message)s"
+)
