abf8292dd9f617ecb7774f68e24e24800ea8d6e4
prakaa <abiprakash007@gmail.com>
2024-10-01 14:13:18 +1000

    programmatically generate generator property tables and add to required tables for building local cache

--- src/ispypsa/data_fetch/local_cache.py
+++ src/ispypsa/data_fetch/local_cache.py
@@ -2,14 +2,52 @@ from pathlib import Path
 
 from isp_workbook_parser import Parser
 
+_ALL_GENERATOR_TYPES = [
+    "existing_generators",
+    "committed_generators",
+    "anticipated_projects",
+    "additional_projects",
+    "new_entrants",
+    "existing_committed_and_anticipated_batteries",
+]
+
+_CONDENSED_GENERATOR_TYPES = [
+    "existing_committed_anticipated_additional_generators",
+    "new_entrants",
+]
+
+
+_GENERATOR_PROPERTIES = {
+    "maximum_capacity": _ALL_GENERATOR_TYPES,
+    "seasonal_ratings": _ALL_GENERATOR_TYPES,
+    "maintenance": ["existing_generators"],
+    "fixed_opex": _CONDENSED_GENERATOR_TYPES,
+    "variable_opex": _CONDENSED_GENERATOR_TYPES,
+    "marginal_loss_factors": _ALL_GENERATOR_TYPES,
+    "auxiliary_load": _CONDENSED_GENERATOR_TYPES,
+}
+
+_GENERATOR_PROPERTY_TABLES = [
+    table_name
+    for key, val in _GENERATOR_PROPERTIES.items()
+    for table_name in [key + "_" + gen_type for gen_type in val]
+]
+
+
 REQUIRED_TABLES = [
     "sub_regional_reference_nodes",
-    "existing_generator_summary",
     "regional_topology_representation",
     "regional_reference_nodes",
     "flow_path_transfer_capability",
     "interconnector_transfer_capability",
-]
+    "existing_generator_summary",
+    "committed_generator_summary",
+    "anticipated_projects_summary",
+    "batteries_summary",
+    "additional_projects_summary",
+    "new_entrants_summary",
+    "expected_closure_years",
+] + _GENERATOR_PROPERTY_TABLES
 
 
 def build_local_cache(cache_path: Path | str, workbook_path: Path | str) -> None:
