5df948f4f5a13c4da429fd32b6ac9e5894c4a922
prakaa <abiprakash007@gmail.com>
2024-10-23 11:48:30 +1100

    add functionality to include alternative lookup and value cols for additional projects

--- src/ispypsa/templater/mappings.py
+++ src/ispypsa/templater/mappings.py
@@ -46,51 +46,65 @@ _ECAA_GENERATOR_STATIC_PROPERTY_TABLE_MAP = {
     "maximum_capacity_mw": dict(
         csv=[f"maximum_capacity_{gen_type}" for gen_type in _ECAA_GENERATOR_TYPES],
         csv_lookup="Generator",
-        csv_values="Installed capacity (MW)",
+        alternative_lookups=["Project"],
+        csv_value="Installed capacity (MW)",
     ),
     "maintenance_duration_%": dict(
         csv="maintenance_existing_generators",
         csv_lookup="Generator type",
-        csv_values="Proportion of time out (%)",
+        csv_value="Proportion of time out (%)",
     ),
     "minimum_load_mw": dict(
         csv="coal_minimum_stable_level",
         csv_lookup="Generating unit",
-        csv_values="Minimum Stable Level (MW)",
+        csv_value="Minimum Stable Level (MW)",
     ),
     "fom_$/kw/annum": dict(
         csv="fixed_opex_existing_committed_anticipated_additional_generators",
         csv_lookup="Generator",
-        csv_values="Fixed OPEX ($/kW/year)",
+        csv_value="Fixed OPEX ($/kW/year)",
     ),
     "vom_$/mwh_sent_out": dict(
         csv="variable_opex_existing_committed_anticipated_additional_generators",
         csv_lookup="Generator",
-        csv_values="Variable OPEX ($/MWh sent out)",
+        csv_value="Variable OPEX ($/MWh sent out)",
     ),
     "heat_rate": dict(
         csv="heat_rates_existing_committed_anticipated_additional_generators",
         csv_lookup="Generator",
-        csv_values="Heat rate (GJ/MWh)",
+        csv_value="Heat rate (GJ/MWh)",
         new_col_name="heat_rate_gj/mwh",
     ),
     "mlf": dict(
         csv=[f"marginal_loss_factors_{gen_type}" for gen_type in _ECAA_GENERATOR_TYPES],
         csv_lookup="Generator",
-        csv_values="MLF",
+        alternative_lookups=["Project"],
+        csv_value="MLF",
+        alternative_values=["MLF - Generation"],
     ),
     "auxiliary_load_%": dict(
         csv="auxiliary_load_existing_committed_anticipated_additional_generators",
         csv_lookup="Fuel/Technology type",
-        csv_values="Auxiliary load (% of nameplate capacity)",
+        csv_value="Auxiliary load (% of nameplate capacity)",
     ),
     "partial_outage_derating_factor_%": dict(
         csv="outages_2023-2024_existing_generators",
         csv_lookup="Fuel type",
-        csv_values="Partial Outage Derating Factor (%)",
+        csv_value="Partial Outage Derating Factor (%)",
     ),
 }
 """
-Existing generators summary table columns mapped to corresponding data CSV and
-lookup information that can be used to retrieve values
+Existing, committed, anticipated and additional summary table columns mapped to
+corresponding data CSV and lookup information that can be used to retrieve values.
+
+    `csv`: A single CSV file name (excluding file extension) or a list of CSV file names
+    `csv_lookup`: Column in the CSV that acts as a key for merging into the summary
+    `alternative_lookups`: A list of alternative key columns, e.g. "Project" as an
+        alternative to  "Generator" in the additional projects table. If a lookup value
+        is NA in the `csv_lookup` column, it will be replaced by a lookup value from this
+        list in the order specified.
+    `csv_value`: Column in the CSV that corresponds to the data to be merged in
+    `alternative_values`: As for `alternative_lookups`, but for the data values in the
+        table, e.g. "MLF - Generation" instead of "MLF" in the additional projects table
+    `new_col_name`: The name that will be used to rename the column in the summary table
 """
