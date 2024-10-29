297a83b5b2732900d853b9205b68b45d81bf8f54
nick-gorman <n.gorman305@gmail.com>
2024-10-29 11:42:11 +1100

    basic translator for ecaa generators

--- src/ispypsa/translator/generators.py
+++ src/ispypsa/translator/generators.py
@@ -2,9 +2,11 @@ from pathlib import Path
 
 import pandas as pd
 
+from ispypsa.translator.mappings import _GENERATOR_ATTRIBUTES
+
 
 def _translate_ecaa_generators(
-    template_path: Path | str,
+    template_path: Path | str,  granularity: str = "sub_regional"
 ) -> pd.DataFrame:
     """Process data on existing, committed, anticipated, and additional (ECAA) generators
     into a format aligned with PyPSA inputs.
@@ -17,3 +19,16 @@ def _translate_ecaa_generators(
     """
     ecaa_generators_template = (
         pd.read_csv(template_path / Path("ecaa_generators_template.csv")))
+
+    if granularity == "sub_regional":
+        _GENERATOR_ATTRIBUTES['sub_region_id'] = "bus"
+    elif granularity == "regional":
+        _GENERATOR_ATTRIBUTES['region_id'] = "bus"
+
+    ecaa_generators_pypsa_format = ecaa_generators_template.loc[:, _GENERATOR_ATTRIBUTES.keys()]
+    ecaa_generators_pypsa_format = ecaa_generators_pypsa_format.rename(columns=_GENERATOR_ATTRIBUTES)
+
+    if granularity == "single_region":
+        ecaa_generators_pypsa_format['bus'] = "NEM"
+
+    return ecaa_generators_pypsa_format
