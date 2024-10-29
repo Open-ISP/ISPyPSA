5519eec82a727a8214852102f09d40dbd608bbff
prakaa <abiprakash007@gmail.com>
2024-10-29 11:05:26 +1100

    break out required tables list into categories, then concatenate

--- src/ispypsa/data_fetch/local_cache.py
+++ src/ispypsa/data_fetch/local_cache.py
@@ -10,22 +10,33 @@ _GENERATOR_PROPERTY_TABLES = [
     for table_name in [key + "_" + gen_type for gen_type in val]
 ]
 
-
-REQUIRED_TABLES = [
+_NETWORK_REQUIRED_TABLES = [
     "sub_regional_reference_nodes",
     "regional_topology_representation",
     "regional_reference_nodes",
     "flow_path_transfer_capability",
     "interconnector_transfer_capability",
+]
+
+_GENERATORS_STORAGE_REQUIRED_SUMMARY_TABLES = [
     "existing_generators_summary",
     "committed_generators_summary",
     "anticipated_projects_summary",
     "batteries_summary",
     "additional_projects_summary",
     "new_entrants_summary",
+]
+
+_GENERATORS_REQUIRED_PROPERTY_TABLES = [
     "expected_closure_years",
     "coal_minimum_stable_level",
 ] + _GENERATOR_PROPERTY_TABLES
+
+REQUIRED_TABLES = (
+    _NETWORK_REQUIRED_TABLES
+    + _GENERATORS_STORAGE_REQUIRED_SUMMARY_TABLES
+    + _GENERATORS_REQUIRED_PROPERTY_TABLES
+)
 
 
 def build_local_cache(cache_path: Path | str, workbook_path: Path | str) -> None:
