f172433b4d0f7c2a8351627f0d532235483c2b82
nick-gorman <n.gorman305@gmail.com>
2024-10-31 13:30:30 +1100

    delete debugging code, add typing, add sub region mapping and rez locations

--- tests/templater/test_renewable_energy_zones.py
+++ tests/templater/test_renewable_energy_zones.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 
-from ispypsa.templater.regions_and_zones import template_region_and_zone_mapping
+from ispypsa.templater.renewable_energy_zones import template_renewable_energy_zone_locations
 
 
-def test_node_templater_regional(workbook_table_cache_test_path: Path):
-    node_template = template_region_and_zone_mapping(workbook_table_cache_test_path)
+def test_renewable_energy_zones_locations(workbook_table_cache_test_path: Path):
+    node_template = template_renewable_energy_zone_locations(workbook_table_cache_test_path)
     assert node_template.index.name == "rez_id"
     assert set(node_template.index) == set(("Q1", "Q2"))
     assert set(node_template.isp_sub_region_id) == set(("NQ", "NQ"))
