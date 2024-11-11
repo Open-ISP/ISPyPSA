7db80ed5da5fd8e418a53ad80b1169ad14e626a0
nick-gorman <n.gorman305@gmail.com>
2024-11-11 14:04:10 +1100

    refactor old fuzzy matching tests

--- tests/templater/test_fuzzy_matching.py
+++ tests/templater/test_fuzzy_matching.py
@@ -3,7 +3,51 @@ import pandas as pd
 from ispypsa.templater.helpers import _fuzzy_match_names
 
 
-def test_sub_region_ids():
+def test_regions() -> None:
+    regions_with_errors_mapped_to_correct_names = {
+        'New South Walks': 'New South Wales',
+        'Coinsland': 'Queensland',
+        'North Australia': 'South Australia',
+        'Bigtoria': 'Victoria',
+        'Radmania': 'Tasmania'
+    }
+    sub_regions_with_errors, correct_names = zip(*regions_with_errors_mapped_to_correct_names.items())
+    matches = _fuzzy_match_names(
+    name_series = pd.Series(sub_regions_with_errors),
+    choices = correct_names,
+    task_desc = 'testing'
+    )
+    assert (matches == pd.Series(correct_names)).all()
+
+
+def test_fuzzy_matching_above_threshold() -> None:
+    regions_with_errors_mapped_to_correct_names = {
+        'New South Walks': 'New South Wales',
+        'Coinsland': 'Queensland',
+        'North Australia': 'South Australia',
+        'Bigtoria': 'Victoria',
+        'Radmania': 'Tasmania'
+    }
+    sub_regions_with_errors, correct_names = zip(*regions_with_errors_mapped_to_correct_names.items())
+    matches = _fuzzy_match_names(
+        name_series = pd.Series(sub_regions_with_errors),
+        choices = correct_names,
+        task_desc = 'testing',
+        threshold = 70
+    )
+    assert (
+        matches
+        == [
+            "New South Wales",
+            "Coinsland",
+            "South Australia",
+            "Victoria",
+            "Tasmania",
+        ]
+    ).all()
+
+
+def test_sub_region_ids() -> None:
     sub_regions_with_errors_mapped_to_correct_names = {
         'Northern Queensland': 'Northern Queensland',
         'Central Queensland': 'Central Queensland',
@@ -27,7 +71,7 @@ def test_sub_region_ids():
     assert (matches == pd.Series(correct_names)).all()
 
 
-def test_generator_names():
+def test_generator_names() -> None:
     generators_with_errors_mapped_to_correct_names = {
         'Bayswater': 'Bayswater',
         'Eraring': 'Eraring',
@@ -47,7 +91,7 @@ def test_generator_names():
     assert (matches == pd.Series(correct_names)).all()
 
 
-def test_abstract():
+def test_abstract() -> None:
     # Even though 'a' is a better match for 'ab' than 'c', 'ab' should still map to 'c' because 'a' is taken.
     abstract_mapping = {
         'a': 'a',
@@ -63,7 +107,7 @@ def test_abstract():
     assert (matches == pd.Series(choices)).all()
 
 
-def test_abstract_to_non_exact_best_match_gets_priority():
+def test_abstract_to_non_exact_best_match_gets_priority() -> None:
     abstract_mapping = {
         'a': 'a',
         'b': 'b',
@@ -79,7 +123,7 @@ def test_abstract_to_non_exact_best_match_gets_priority():
     assert (matches == pd.Series(choices)).all()
 
 
-def test_abstract_threshold():
+def test_abstract_threshold() -> None:
     # With a threshold of 90 'ab' is not allowed to match with 'c' and so defaults to matching with itself.
     abstract_mapping = {
         'a': 'a',
@@ -101,7 +145,7 @@ def test_abstract_threshold():
     assert (matches == pd.Series(correct_answers)).all()
 
 
-def test_abstract_threshold_no_match():
+def test_abstract_threshold_no_match() -> None:
     # With a threshold of 90 'ab' is not allowed to match with 'c' and with no_match set to 'No Match', the
     # match for 'ab' should return as 'No Match'.
     abstract_mapping = {
@@ -125,7 +169,7 @@ def test_abstract_threshold_no_match():
     assert (matches == pd.Series(correct_answers)).all()
 
 
-def test_abstract_run_out_of_choices():
+def test_abstract_run_out_of_choices() -> None:
     # If there aren't enough choice resorts to the no_match mode (which by default is to match with self)
     abstract_mapping = {
         'a': 'a',
