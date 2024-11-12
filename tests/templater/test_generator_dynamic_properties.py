1dc7d4dda1fc60aa9f2f9a7954a3a49093c2006a
prakaa <abiprakash007@gmail.com>
2024-11-13 09:39:11 +1100

    testing dynamic properties now handles liquid fuel prices being a series

--- tests/templater/test_generator_dynamic_properties.py
+++ tests/templater/test_generator_dynamic_properties.py
@@ -11,7 +11,9 @@ def test_generator_dynamic_properties_templater(workbook_table_cache_test_path: 
         mapped_dfs = template_generator_dynamic_properties(
             workbook_table_cache_test_path, scenario
         )
-        for key, df in mapped_dfs.items():
+        for key, df in [
+            item for item in mapped_dfs.items() if item[0] != "liquid_fuel_prices"
+        ]:
             if "price" in key:
                 assert all("$/gj" in col for col in df.columns[1:])
                 assert all(df.iloc[:, 1:].dtypes != "object")
@@ -22,3 +24,7 @@ def test_generator_dynamic_properties_templater(workbook_table_cache_test_path: 
             elif "ratings" in key:
                 assert all(df.iloc[:, 3:].dtypes != "object")
                 assert all(df.notna())
+        liquid_fuel = mapped_dfs["liquid_fuel_prices"]
+        assert all("$/gj" in ind for ind in liquid_fuel.index)
+        assert all(liquid_fuel.notna())
+        assert liquid_fuel.dtypes != "object"
