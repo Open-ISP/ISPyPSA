beeff6395041ab87cf6ec4b60fd20bba09972dd0
nick-gorman <n.gorman305@gmail.com>
2024-11-20 09:50:18 +1100

    add trace config to pydantic model and fix regional trace aggregation

--- tests/config/test_pydantic_model_config.py
+++ tests/config/test_pydantic_model_config.py
@@ -8,15 +8,65 @@ from ispypsa.config.validators import ModelConfig
     "scenario", ["Step Change", "Progressive Change", "Green Energy Exports"]
 )
 @pytest.mark.parametrize("granularity", ["sub_regional", "regional", "single_region"])
-def test_valid_granularity(scenario, granularity):
-    ModelConfig(**{"scenario": scenario, "network": {"granularity": granularity}})
+@pytest.mark.parametrize("year_type", ["fy", "calendar"])
+def test_valid_config(scenario, granularity, year_type):
+    ModelConfig(
+        **{
+            "scenario": scenario,
+            "network": {"granularity": granularity},
+            "traces": {
+                "year_type": year_type,
+                "start_year": 2025,
+                "end_year": 2026,
+                "reference_year_cycle": [2018],
+            },
+        }
+    )
 
 
 def test_invalid_scenario():
     with pytest.raises(ValidationError):
-        ModelConfig(**{"scenario": "BAU", "network": {"granularity": "Step Change"}})
+        ModelConfig(
+            **{
+                "scenario": "BAU",
+                "network": {"granularity": "sub_regional"},
+                "traces": {
+                    "year_type": "fy",
+                    "start_year": 2025,
+                    "end_year": 2026,
+                    "reference_year_cycle": [2018],
+                },
+            }
+        )
 
 
 def test_invalid_granularity():
     with pytest.raises(ValidationError):
-        ModelConfig(**{"network": {"granularity": "wastlands"}})
+        ModelConfig(
+            **{
+                "scenario": "Step Change",
+                "network": {"granularity": "wastelands"},
+                "traces": {
+                    "year_type": "fy",
+                    "start_year": 2025,
+                    "end_year": 2026,
+                    "reference_year_cycle": [2018],
+                },
+            }
+        )
+
+
+def test_invalid_end_year():
+    with pytest.raises(ValueError):
+        ModelConfig(
+            **{
+                "scenario": "Step Change",
+                "network": {"granularity": "sub_regional"},
+                "traces": {
+                    "year_type": "fy",
+                    "start_year": 2025,
+                    "end_year": 2024,
+                    "reference_year_cycle": [2018],
+                },
+            }
+        )
