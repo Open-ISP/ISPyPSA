d912c8496632a9e2ad992541c5ddd8a2f22ac05b
nick-gorman <n.gorman305@gmail.com>
2024-11-19 13:11:34 +1100

    add tests for scenario validation

--- tests/config/test_pydantic_model_config.py
+++ tests/config/test_pydantic_model_config.py
@@ -4,9 +4,17 @@ from pydantic import ValidationError
 from ispypsa.config.validators import ModelConfig
 
 
+@pytest.mark.parametrize(
+    "scenario", ["Step Change", "Progressive Change", "Green Energy Exports"]
+)
 @pytest.mark.parametrize("granularity", ["sub_regional", "regional", "single_region"])
-def test_valid_granularity(granularity):
-    ModelConfig(**{"network": {"granularity": granularity}})
+def test_valid_granularity(scenario, granularity):
+    ModelConfig(**{"scenario": scenario, "network": {"granularity": granularity}})
+
+
+def test_invalid_scenario():
+    with pytest.raises(ValidationError):
+        ModelConfig(**{"scenario": "BAU", "network": {"granularity": "Step Change"}})
 
 
 def test_invalid_granularity():
