7837dcdbf634687d7f7ba6fe28db3a5a118fb8bc
prakaa <abiprakash007@gmail.com>
2024-11-26 16:09:24 +1100

    add solver name and options to run

--- src/ispypsa/model/run.py
+++ src/ispypsa/model/run.py
@@ -1,10 +1,13 @@
 import pypsa
 
 
-def run(network: pypsa.Network) -> None:
+def run(network: pypsa.Network, solver_name="highs", solver_options={}) -> None:
     """Runs the model by calling `optimize()` on the `pypsa.Network`
 
     Args:
         network: The `pypsa.Network` object
+        solver_name: PyPSA/linopy-compatible solver. See
+            https://pypsa.readthedocs.io/en/latest/getting-started/installation.html
+        solver_options: Options to pass to the solver.
     """
-    network.optimize()
+    network.optimize(solver_name=solver_name, solver_options=solver_options)
