97e04fa932f0ab34fbc72b26a54c26be0eb55cf4
nick-gorman <n.gorman305@gmail.com>
2025-02-17 12:23:23 +1100

    add tests for bus timeseries and custom constraint translater

--- tests/test_translator/test_buses.py
+++ tests/test_translator/test_buses.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pandas as pd
 
 from ispypsa.translator.buses import (
@@ -5,7 +7,9 @@ from ispypsa.translator.buses import (
     _translate_isp_sub_regions_to_buses,
     _translate_nem_regions_to_buses,
     _translate_rezs_to_buses,
+    create_pypsa_friendly_bus_demand_timeseries,
 )
+from ispypsa.translator.snapshot import _create_complete_snapshots_index
 
 
 def test_translate_isp_sub_regions_to_buses():
@@ -47,3 +51,169 @@ def test_translate_rezs_to_buses():
     )
     buses = _translate_rezs_to_buses(rezs)
     pd.testing.assert_frame_equal(buses, expected_buses)
+
+
+def test_create_pypsa_friendly_bus_timeseries_data_sub_regions(tmp_path):
+    parsed_trace_path = Path(__file__).parent.parent / Path("trace_data")
+
+    sub_regions_ispypsa = pd.DataFrame(
+        {
+            "isp_sub_region_id": ["CNSW", "NNSW", "CQ", "NQ"],
+            "nem_region_id": ["NSW", "NSW", "QLD", "QLD"],
+        }
+    )
+
+    snapshots = _create_complete_snapshots_index(
+        start_year=2025,
+        end_year=2026,
+        operational_temporal_resolution_min=30,
+        year_type="fy",
+    )
+
+    create_pypsa_friendly_bus_demand_timeseries(
+        sub_regions_ispypsa,
+        parsed_trace_path,
+        tmp_path,
+        scenario="Step Change",
+        regional_granularity="sub_regions",
+        reference_year_mapping={2025: 2011, 2026: 2018},
+        year_type="fy",
+        snapshot=snapshots,
+    )
+
+    files = [
+        "demand/Step_Change/RefYear2011/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_CNSW_POE50_OPSO_MODELLING_HalfYear2024-2.parquet",
+        "demand/Step_Change/RefYear2011/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_CNSW_POE50_OPSO_MODELLING_HalfYear2025-1.parquet",
+        "demand/Step_Change/RefYear2018/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_CNSW_POE50_OPSO_MODELLING_HalfYear2025-2.parquet",
+        "demand/Step_Change/RefYear2018/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_CNSW_POE50_OPSO_MODELLING_HalfYear2026-1.parquet",
+    ]
+
+    files = [parsed_trace_path / Path(file) for file in files]
+
+    expected_trace = pd.concat([pd.read_parquet(file) for file in files])
+    expected_trace["Datetime"] = expected_trace["Datetime"].astype("datetime64[ns]")
+    expected_trace = expected_trace.reset_index(drop=True)
+
+    got_trace = pd.read_parquet(tmp_path / Path("demand_traces/CNSW.parquet"))
+
+    pd.testing.assert_frame_equal(expected_trace, got_trace)
+
+
+def test_create_pypsa_friendly_bus_timeseries_data_nem_regions(tmp_path):
+    parsed_trace_path = Path(__file__).parent.parent / Path("trace_data")
+
+    sub_regions_ispypsa = pd.DataFrame(
+        {
+            "isp_sub_region_id": ["CNSW", "NNSW", "CQ", "NQ"],
+            "nem_region_id": ["NSW", "NSW", "QLD", "QLD"],
+        }
+    )
+
+    snapshots = _create_complete_snapshots_index(
+        start_year=2025,
+        end_year=2026,
+        operational_temporal_resolution_min=30,
+        year_type="fy",
+    )
+
+    create_pypsa_friendly_bus_demand_timeseries(
+        sub_regions_ispypsa,
+        parsed_trace_path,
+        tmp_path,
+        scenario="Step Change",
+        regional_granularity="nem_regions",
+        reference_year_mapping={2025: 2011, 2026: 2018},
+        year_type="fy",
+        snapshot=snapshots,
+    )
+
+    files = [
+        "demand/Step_Change/RefYear2011/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_CNSW_POE50_OPSO_MODELLING_HalfYear2024-2.parquet",
+        "demand/Step_Change/RefYear2011/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_CNSW_POE50_OPSO_MODELLING_HalfYear2025-1.parquet",
+        "demand/Step_Change/RefYear2018/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_CNSW_POE50_OPSO_MODELLING_HalfYear2025-2.parquet",
+        "demand/Step_Change/RefYear2018/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_CNSW_POE50_OPSO_MODELLING_HalfYear2026-1.parquet",
+        "demand/Step_Change/RefYear2011/NNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_NNSW_POE50_OPSO_MODELLING_HalfYear2024-2.parquet",
+        "demand/Step_Change/RefYear2011/NNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_NNSW_POE50_OPSO_MODELLING_HalfYear2025-1.parquet",
+        "demand/Step_Change/RefYear2018/NNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_NNSW_POE50_OPSO_MODELLING_HalfYear2025-2.parquet",
+        "demand/Step_Change/RefYear2018/NNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_NNSW_POE50_OPSO_MODELLING_HalfYear2026-1.parquet",
+    ]
+
+    files = [parsed_trace_path / Path(file) for file in files]
+
+    expected_trace = pd.concat([pd.read_parquet(file) for file in files])
+
+    expected_trace["Datetime"] = expected_trace["Datetime"].astype("datetime64[ns]")
+
+    expected_trace = expected_trace.groupby("Datetime", as_index=False).agg(
+        {"Value": "sum"}
+    )
+
+    expected_trace = expected_trace.reset_index(drop=True)
+
+    got_trace = pd.read_parquet(tmp_path / Path("demand_traces/NSW.parquet"))
+
+    pd.testing.assert_frame_equal(expected_trace, got_trace)
+
+
+def test_create_pypsa_friendly_bus_timeseries_data_single_region(tmp_path):
+    parsed_trace_path = Path(__file__).parent.parent / Path("trace_data")
+
+    sub_regions_ispypsa = pd.DataFrame(
+        {
+            "isp_sub_region_id": ["CNSW", "NNSW", "CQ", "NQ"],
+            "nem_region_id": ["NSW", "NSW", "QLD", "QLD"],
+        }
+    )
+
+    snapshots = _create_complete_snapshots_index(
+        start_year=2025,
+        end_year=2026,
+        operational_temporal_resolution_min=30,
+        year_type="fy",
+    )
+
+    create_pypsa_friendly_bus_demand_timeseries(
+        sub_regions_ispypsa,
+        parsed_trace_path,
+        tmp_path,
+        scenario="Step Change",
+        regional_granularity="single_region",
+        reference_year_mapping={2025: 2011, 2026: 2018},
+        year_type="fy",
+        snapshot=snapshots,
+    )
+
+    files = [
+        "demand/Step_Change/RefYear2011/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_CNSW_POE50_OPSO_MODELLING_HalfYear2024-2.parquet",
+        "demand/Step_Change/RefYear2011/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_CNSW_POE50_OPSO_MODELLING_HalfYear2025-1.parquet",
+        "demand/Step_Change/RefYear2018/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_CNSW_POE50_OPSO_MODELLING_HalfYear2025-2.parquet",
+        "demand/Step_Change/RefYear2018/CNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_CNSW_POE50_OPSO_MODELLING_HalfYear2026-1.parquet",
+        "demand/Step_Change/RefYear2011/NNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_NNSW_POE50_OPSO_MODELLING_HalfYear2024-2.parquet",
+        "demand/Step_Change/RefYear2011/NNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2011_NNSW_POE50_OPSO_MODELLING_HalfYear2025-1.parquet",
+        "demand/Step_Change/RefYear2018/NNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_NNSW_POE50_OPSO_MODELLING_HalfYear2025-2.parquet",
+        "demand/Step_Change/RefYear2018/NNSW/POE50/OPSO_MODELLING/Step_Change_RefYear2018_NNSW_POE50_OPSO_MODELLING_HalfYear2026-1.parquet",
+        "demand/Step_Change/RefYear2011/CQ/POE50/OPSO_MODELLING/Step_Change_RefYear2011_CQ_POE50_OPSO_MODELLING_HalfYear2024-2.parquet",
+        "demand/Step_Change/RefYear2011/CQ/POE50/OPSO_MODELLING/Step_Change_RefYear2011_CQ_POE50_OPSO_MODELLING_HalfYear2025-1.parquet",
+        "demand/Step_Change/RefYear2018/CQ/POE50/OPSO_MODELLING/Step_Change_RefYear2018_CQ_POE50_OPSO_MODELLING_HalfYear2025-2.parquet",
+        "demand/Step_Change/RefYear2018/CQ/POE50/OPSO_MODELLING/Step_Change_RefYear2018_CQ_POE50_OPSO_MODELLING_HalfYear2026-1.parquet",
+        "demand/Step_Change/RefYear2011/NQ/POE50/OPSO_MODELLING/Step_Change_RefYear2011_NQ_POE50_OPSO_MODELLING_HalfYear2024-2.parquet",
+        "demand/Step_Change/RefYear2011/NQ/POE50/OPSO_MODELLING/Step_Change_RefYear2011_NQ_POE50_OPSO_MODELLING_HalfYear2025-1.parquet",
+        "demand/Step_Change/RefYear2018/NQ/POE50/OPSO_MODELLING/Step_Change_RefYear2018_NQ_POE50_OPSO_MODELLING_HalfYear2025-2.parquet",
+        "demand/Step_Change/RefYear2018/NQ/POE50/OPSO_MODELLING/Step_Change_RefYear2018_NQ_POE50_OPSO_MODELLING_HalfYear2026-1.parquet",
+    ]
+
+    files = [parsed_trace_path / Path(file) for file in files]
+
+    expected_trace = pd.concat([pd.read_parquet(file) for file in files])
+
+    expected_trace["Datetime"] = expected_trace["Datetime"].astype("datetime64[ns]")
+
+    expected_trace = expected_trace.groupby("Datetime", as_index=False).agg(
+        {"Value": "sum"}
+    )
+
+    expected_trace = expected_trace.reset_index(drop=True)
+
+    got_trace = pd.read_parquet(tmp_path / Path("demand_traces/NEM.parquet"))
+
+    pd.testing.assert_frame_equal(expected_trace, got_trace)
