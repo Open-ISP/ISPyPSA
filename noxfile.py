6b2515593441ca0499f379e785d2a43ab9322d0b
Dylan McConnell <dylanjmcconnell@users.noreply.github.com>
2026-02-12 23:57:34 +1100

    Downgrade to <3.14 again - similar issue with a shapely on macos

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 
 nox.options.default_venv_backend = "uv"
 
-PYTHON_VERSIONS = ["3.11", "3.12", "3.13", "3.14"]
+PYTHON_VERSIONS = ["3.11", "3.12", "3.13"]
 
 
 @nox.session(python=PYTHON_VERSIONS)
